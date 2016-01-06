65c9b5e363de12b0ba17f253a3b9c593e9dd2d5e
yang <ysfnevercry@gmail.com>
2016-01-06 22:53:21 +0800

    Add Grunt
    学习Node.js 第二期建站Demo

--- app.js
+++ app.js
@@ -1,71 +1,122 @@
 var express = require('express')
 var path = require('path')
+var mongoose = require('mongoose')
+var _ = require('underscore')
+var Movie = require('./models/movie')
 var port = process.env.PORT || 3000
 var app = express()
+var bodyParser = require('body-parser')
+
+
+mongoose.connect('mongodb://localhost/imooc')
+
 
 app.set('views', './views/pages')
 app.set('view engine', 'jade')
-app.use(express.static(path.join(__dirname, 'bower_components')))
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(bodyParser.urlencoded({extended: true}))
+app.locals.moment = require('moment')
 app.listen(port)
+
 
 console.log('imooc start on port ' + port)
 
 // index page 
 app.get('/', function(req, res) {
-	res.render('index', {
-		title: 'imooc 首页',
-		movies: [{
-			title: '机械战警',
-			_id: 1,
-			poster: 'http://img31.mtime.cn/mg/2014/02/26/102130.26283987_270X405X4.jpg'
-	},{
-			title: '机械战警',
-			_id: 2,
-			poster: 'http://img31.mtime.cn/mg/2014/02/26/102130.26283987_270X405X4.jpg'
-	},{
-			title: '机械战警',
-			_id: 3,
-			poster: 'http://img31.mtime.cn/mg/2014/02/26/102130.26283987_270X405X4.jpg'
-	},{
-			title: '机械战警',
-			_id: 4,
-			poster: 'http://img31.mtime.cn/mg/2014/02/26/102130.26283987_270X405X4.jpg'
-		}]
+	Movie.fetch(function(err, movies){
+		if (err) {
+			console.log(err)
+		}
+
+		res.render('index', {
+			title: 'imooc 首页',
+			movies: movies
+		})
 	})
 })
 
 // list page 
 app.get('/admin/list', function(req, res) {
-	res.render('list', {
-		title: 'imooc 列表页',
-		movies: [{
-			title: '机械战警',
-			_id: 1,
-			doctor: '何塞.帕迪利亚',
-			country: '美国',
-			year: 2014,
-			language: '英语',
-			flash: 'http://player.youku.com/player.php/sid/XNjYxNTUzNjIw/v.swf',
-			summary: '2028年，专事军火开发的机器人公司Omni Corp.生产了大量装备精良的机械战警，他们被投入到惩治犯罪等行动中，取得显著的效果。罪犯横行的底特律市，嫉恶如仇、正义感十足的警察亚历克斯·墨菲（乔尔·金纳曼 饰）遭到仇家暗算，身体受到毁灭性破坏。借助于Omni公司天才博士丹尼特·诺顿（加里·奥德曼 饰）最前沿的技术，墨菲以机械战警的形态复活。数轮严格的测试表明，墨菲足以承担起维护社会治安的重任，他的口碑在民众中直线飙升，而墨菲的妻子克拉拉（艾比·考尼什 饰）和儿子大卫却再难从他身上感觉亲人的温暖。感知到妻儿的痛苦，墨菲决心向策划杀害自己的犯罪头子展开反击'
-		}]
+	Movie.fetch(function(err, movies){
+		if (err) {
+			console.log(err)
+		}
+
+		res.render('list', {
+			title: 'imooc 列表页',
+			movies: movies
+		})
 	})
+})
+
+// list delete movie
+app.delete('/admin/list', function(req, res) {
+	var id = req.query.id
+
+	if (id) {
+		Movie.remove({_id: id}, function(err, movie) {
+			if (err) {
+				console.log(err) 
+			} else {
+				res.json({success: 1})
+			}
+		})
+	}
 })
 
 // detail page 
 app.get('/movie/:id', function(req, res) {
-	res.render('detail', {
-		title: 'imooc 详情页',
-		movie: {
-			doctor: '何塞.帕迪利亚',
-			country: '美国',
-			title: '机械战警',
-			year: 2014,
-			poster: 'http://img31.mtime.cn/mg/2014/02/26/102130.26283987_270X405X4.jpg',
-			language: '英语',
-			flash: 'http://player.youku.com/player.php/sid/XNjYxNTUzNjIw/v.swf',
-			summary: '2028年，专事军火开发的机器人公司Omni Corp.生产了大量装备精良的机械战警，他们被投入到惩治犯罪等行动中，取得显著的效果。罪犯横行的底特律市，嫉恶如仇、正义感十足的警察亚历克斯·墨菲（乔尔·金纳曼 饰）遭到仇家暗算，身体受到毁灭性破坏。借助于Omni公司天才博士丹尼特·诺顿（加里·奥德曼 饰）最前沿的技术，墨菲以机械战警的形态复活。数轮严格的测试表明，墨菲足以承担起维护社会治安的重任，他的口碑在民众中直线飙升，而墨菲的妻子克拉拉（艾比·考尼什 饰）和儿子大卫却再难从他身上感觉亲人的温暖。感知到妻儿的痛苦，墨菲决心向策划杀害自己的犯罪头子展开反击'
-		}
+	var id = req.params.id
+
+	Movie.findById(id, function(err, movie) {
+		res.render('detail', {
+			title: movie.title,
+			movie: movie
+		})
 	})
+})
+
+// admin post movie
+app.post('/admin/movie/new', function(req, res) {
+	var id = req.body.movie._id
+	var movieObj = req.body.movie
+	var _movie
+
+	if (id !== 'undefined') {
+		Movie.findById(id, function(err, movie) {
+			if (err) {
+				console.log(err)
+			}
+
+			_movie = _.extend(movie, movieObj)
+			_movie.save(function(err, movie) {
+				if (err) {
+					console.log(err)
+				}
+
+				res.redirect('/movie/' + movie._id)
+			})
+		})
+	} else {
+		_movie = new Movie ({
+			doctor: movieObj.doctor,
+			title: movieObj.title,
+			country: movieObj.country,
+			language: movieObj.language,
+			year: movieObj.year,
+			poster: movieObj.poster,
+			summary: movieObj.summary,
+			flash: movieObj.flash
+		})
+
+		_movie.save(function(err, movie) {
+			if (err) {
+					console.log(err)
+				}
+
+			res.redirect('/movie/' + movie._id)
+		})
+	} 	
 })
 
 // admin page 
@@ -83,4 +134,18 @@ app.get('/admin/movie', function(req, res) {
 			language: ''
 		}
 	})
+})
+
+// admin update  movie
+app.get('/admin/update/:id', function(req, res) {
+	var id = req.params.id
+
+	if (id) {
+		Movie.findById(id, function(err, movie) {
+			res.render('admin', {
+				title: 'imooc 后台更新页',
+				movie: movie
+			})
+		})
+	}
 })
