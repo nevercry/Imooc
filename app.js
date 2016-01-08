f3cf0e22364907824981778cda1a78205aff3737
yang <ysfnevercry@gmail.com>
2016-01-08 23:57:27 +0800

    Add User Role Update Project Structure

--- app.js
+++ app.js
@@ -3,21 +3,17 @@ var path = require('path')
 var mongoose = require('mongoose')
 var session = require('express-session')
 var mongoStore = require('connect-mongo')(session)
-var _ = require('underscore')
-var Movie = require('./models/movie')
-var User = require('./models/User')
+var morgan = require('morgan')
+
 var port = process.env.PORT || 3000
 var bodyParser = require('body-parser')
 var cookieParser = require('cookie-parser')
 var app = express()
 var dbUrl = 'mongodb://localhost/imooc'
 
-
-
 mongoose.connect(dbUrl)
 
-
-app.set('views', './views/pages')
+app.set('views', './app/views/pages')
 app.set('view engine', 'jade')
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(bodyParser.urlencoded({extended: true}))
@@ -31,223 +27,17 @@ app.use(session({
 		collection: 'sessions'
 	})
 }))
+
+if ('development' === app.get('env')) {
+	app.set('showStackError', true)
+	app.use(morgan(':method :url :status'))
+	app.locals.pretty = true
+	mongoose.set('debug', true)
+}
+
+require('./config/routes')(app)
+
 app.locals.moment = require('moment')
 app.listen(port)
 
-
 console.log('imooc start on port ' + port)
-
-// index page 
-app.get('/', function(req, res) {
-	console.log('user in session: ')
-	console.log(req.session.user)
-
-	var _user = req.session.user
-
-	if (_user) {
-		app.locals.user = _user
-	}
-
-	Movie.fetch(function(err, movies){
-		if (err) {
-			console.log(err)
-		}
-
-		res.render('index', {
-			title: 'imooc 首页',
-			movies: movies
-		})
-	})
-})
-
-// signup
-app.post('/user/signup', function(req, res) {
-	var _user = req.body.user
-
-
-	User.find({name: _user.name}, function(err, user) {
-		if (err) {
-			console.log(err)
-		}
-
-		if (user) {
-			return res.redirect('/')
-		} else {
-			var user = new User(_user)
-
-			user.save(function(err, user) {
-				if (err) {
-					console.log(err)
-				}
-
-				res.redirect('/admin/userlist')
-			})
-		}
-	})
-})
-
-// Login 
-app.post('/user/login', function(req, res) {
-	var _user = req.body.user
-	var name = _user.name
-	var password = _user.password
-
-	User.findOne({name: name}, function(err, user) {
-		if (err) {
-			console.log(err)
-		}
-
-		if (!user) {
-			return res.redirect('/')
-		} 
-
-		user.comparePassword(password, function(err, isMatch) {
-			if (err) {
-				console.log(err)
-			}
-
-			if (isMatch) {
-				req.session.user = user
-				return res.redirect('/')
-			} else {
-				console.log('Password is not matched')
-			}
-		})
-	})
-})
-
-// logout
-app.get('/logout', function(req, res) {
-	delete req.session.user 
-	delete app.locals.user
-	res.redirect('/')
-})
-
-// user list page 
-app.get('/admin/userlist', function(req, res) {
-	User.fetch(function(err, users){
-		if (err) {
-			console.log(err)
-		}
-
-		res.render('userlist', {
-			title: 'imooc 用户列表页',
-			users: users
-		})
-	})
-})
-
-// list page 
-app.get('/admin/list', function(req, res) {
-	Movie.fetch(function(err, movies){
-		if (err) {
-			console.log(err)
-		}
-
-		res.render('list', {
-			title: 'imooc 列表页',
-			movies: movies
-		})
-	})
-})
-
-// list delete movie
-app.delete('/admin/list', function(req, res) {
-	var id = req.query.id
-
-	if (id) {
-		Movie.remove({_id: id}, function(err, movie) {
-			if (err) {
-				console.log(err) 
-			} else {
-				res.json({success: 1})
-			}
-		})
-	}
-})
-
-// detail page 
-app.get('/movie/:id', function(req, res) {
-	var id = req.params.id
-
-	Movie.findById(id, function(err, movie) {
-		res.render('detail', {
-			title: movie.title,
-			movie: movie
-		})
-	})
-})
-
-// admin post movie
-app.post('/admin/movie/new', function(req, res) {
-	var id = req.body.movie._id
-	var movieObj = req.body.movie
-	var _movie
-
-	if (id !== 'undefined') {
-		Movie.findById(id, function(err, movie) {
-			if (err) {
-				console.log(err)
-			}
-
-			_movie = _.extend(movie, movieObj)
-			_movie.save(function(err, movie) {
-				if (err) {
-					console.log(err)
-				}
-
-				res.redirect('/movie/' + movie._id)
-			})
-		})
-	} else {
-		_movie = new Movie ({
-			doctor: movieObj.doctor,
-			title: movieObj.title,
-			country: movieObj.country,
-			language: movieObj.language,
-			year: movieObj.year,
-			poster: movieObj.poster,
-			summary: movieObj.summary,
-			flash: movieObj.flash
-		})
-
-		_movie.save(function(err, movie) {
-			if (err) {
-					console.log(err)
-				}
-
-			res.redirect('/movie/' + movie._id)
-		})
-	} 	
-})
-
-// admin page 
-app.get('/admin/movie', function(req, res) {
-	res.render('admin', {
-		title: 'imooc 后台录入页',
-		movie: {
-			title: '',
-			doctor: '',
-			country: '',
-			year: '',
-			poster: '',
-			flash: '',
-			summary: '',
-			language: ''
-		}
-	})
-})
-
-// admin update  movie
-app.get('/admin/update/:id', function(req, res) {
-	var id = req.params.id
-
-	if (id) {
-		Movie.findById(id, function(err, movie) {
-			res.render('admin', {
-				title: 'imooc 后台更新页',
-				movie: movie
-			})
-		})
-	}
-})
---
+++ config/routes.js
@@ -0,0 +1,36 @@
+var Index = require('../app/controllers/index')
+var User = require('../app/controllers/user')
+var Movie = require('../app/controllers/movie')
+
+
+module.exports = function(app) {
+
+	// pre handle user
+	app.use(function(req, res, next) {
+		var _user = req.session.user
+
+		app.locals.user = _user
+
+		next()
+	})
+
+	// index 
+	app.get('/', Index.index)
+
+	// User
+	app.post('/user/signup', User.signup)
+	app.post('/user/login', User.login)
+	app.get('/login', User.showLogin)
+	app.get('/signup', User.showSignup)
+	app.get('/logout', User.logout)
+	app.get('/admin/user/list', User.loginRequired, User.adminRequired, User.list)
+
+	// Movie
+	app.get('/movie/:id', Movie.detail)
+	app.get('/admin/movie/new', User.loginRequired, User.adminRequired, Movie.new)
+	app.get('/admin/movie/update/:id', User.loginRequired, User.adminRequired, Movie.update)
+	app.post('/admin/movie/', User.loginRequired, User.adminRequired, Movie.save)
+	app.get('/admin/movie/list', User.loginRequired, User.adminRequired, Movie.list)
+	app.delete('/admin/movie/list', User.loginRequired, User.adminRequired, Movie.del)
+}
+
