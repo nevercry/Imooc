68ef1aa36047d4e6ba701e596058e4ead953f2d9
yang <ysfnevercry@gmail.com>
2016-01-12 17:32:40 +0800

    Add Search

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -21,31 +21,53 @@ exports.index = function(req, res) {
 // search page 
 exports.search = function(req, res) {
 	var catId = req.query.cat
-	var page = Number(req.query.p)
+	var q = req.query.q
+	var page = parseInt(req.query.p, 10) || 0
 	var count = 2
 	var index = page * count
 
-	Category
-		.find({_id: catId})
-		.populate({
-			path: 'movies',
-		})
-		.exec(function(err, categories) {
-			if (err) {
-			console.log(err)
-		}
+	if (catId) {
+		Category
+			.find({_id: catId})
+			.populate({
+				path: 'movies',
+			})
+			.exec(function(err, categories) {
+				if (err) {
+					console.log(err)
+				}
 
-		var category = categories[0] || {}
-		var movies = category.movies || []
-		var results = movies.slice(index, index + count)
+			var category = categories[0] || {}
+			var movies = category.movies || []
+			var results = movies.slice(index, index + count)
 
-		res.render('results', {
-			title: 'imooc 结果列表页',
-			keyword: category.name,
-			currentPage: (1 + page),
-			query: 'cat=' + catId,
-			totalPage: Math.ceil(movies.length / count),
-			movies: results
+			res.render('results', {
+				title: 'imooc 结果列表页',
+				keyword: category.name,
+				currentPage: (page + 1),
+				query: 'cat=' + catId,
+				totalPage: Math.ceil(movies.length / count),
+				movies: results
+			})
+		})		
+	}
+	else {
+		Movie
+			.find({title: new RegExp(q + '.*', 'i')})
+			.exec(function(err, movies) {
+				if (err) {
+				console.log(err)
+				}
+			var results = movies.slice(index, index + count)
+
+			res.render('results', {
+				title: 'imooc 结果列表页',
+				keyword: q,
+				currentPage: (page + 1),
+				query: 'q=' + q,
+				totalPage: Math.ceil(movies.length / count),
+				movies: results
+			})
 		})
-	})		
+	}
 }
